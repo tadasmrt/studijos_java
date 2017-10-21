fc75a8b3d4ff7528a619ebac369cac3e67707dc1
tadasmrt <tadasmart@gmail.com>
2017-10-21 18:33:00 +0300

    Update

--- src/metodai/Uzd38.java
+++ src/metodai/Uzd38.java
@@ -1,14 +1,15 @@
 package metodai;
-import java.util.Scanner;
+
 public class Uzd38 {
 
 	public static void main(String[] args) {
-		int a = 8;
+		printText(7);
+	}
+	
+	public static void printText(int tekstas) {
+		for (int i = 0; i <= tekstas; i++) {
 		System.out.println("The swamp, the hoe and Java");
-	}
-	public static void printText() {
-		int a;
-		for (int  i=0; i<=a; i++);
-		System.out.println("" + i);
-	}
+		}
+	System.out.println();
+}
 }
--- src/metodai/Uzd40.java
+++ src/metodai/Uzd40.java
@@ -1,6 +1,6 @@
 package metodai;
 
-public class Uzd39_4 {
+public class Uzd40 {
 
 	public static void main(String[] args) {
 		printTriangle(8);
@@ -8,14 +8,19 @@ public class Uzd39_4 {
 	
 	public static void printTriangle(int size) {
 		for(int i = 1; i <= size; i++) {
-			printLine(i, size - i);
+			printLine(i * 2 - 1, size - i);
 		}
 	}
 	
 	public static void printLine(int star, int empty) {
-		for(int i = 1; i <= star; i++ ) {
+		for(int i = 1; i <= empty; i++) {
+			System.out.print(" ");
+		}
+
+		for(int i = 1; i <= star; i++) {
 			System.out.print("*");
 		}
 		System.out.println();
 	}
+	
 }
